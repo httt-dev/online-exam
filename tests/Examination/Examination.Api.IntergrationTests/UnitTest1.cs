8ccf905eadbbe4a3b2adb4700d92f13f12cb703b
hoanguyen <httt.dev@gmail.com>
2021-07-10 16:20:36 +0700

    Create solution

---
+++ tests/Examination/Examination.Api.IntergrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Examination.Api.IntergrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
